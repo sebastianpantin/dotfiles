696dd816a11217ce049fe8e2c3f5b4f04a78545d
Sebastian Pantin Liljevall <sebastian.p.liljevall@captario.com>
2024-08-20 14:08:35 +0200

    add aerial

--- .config/nvim/lua/plugins/aerial.lua
+++ .config/nvim/lua/plugins/aerial.lua
@@ -1,9 +1,19 @@
 return {
-    'stevearc/aerial.nvim',
+    "stevearc/aerial.nvim",
     opts = {},
     -- Optional dependencies
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
-        "nvim-tree/nvim-web-devicons"
+        "nvim-tree/nvim-web-devicons",
     },
+    config = function ()
+        local map = require("helpers.keys").map
+        require("aerial").setup({
+            on_attach = function()
+                map("n", "<leader>a", "<cmd>AerialToggle!<CR>", "Toggle aerial")
+                vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+            end
+        })
+        
+    end
 }
